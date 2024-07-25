77f0e89a5cf43f415e7b98e7a47b0cbb26cdf697
Abhishek Gupta <abhisheknew2023@gmail.com>
2024-07-26 00:34:16 +0530

    bug fixed

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Image from 'next/image';
 import Hero from '@/components/core/Hero';
 
+
 const Home = () => {
   return (
 
@@ -10,6 +11,7 @@ const Home = () => {
       <div className='w-full'>
 
         <Hero />
+
 
 
 
--- components/core/Hero.tsx
+++ components/core/Hero.tsx
@@ -6,8 +6,8 @@ import Particles from '../ui/Particles'
 const Hero = () => {
     return (
         <>
-            <Particles />
             <div className="min-h-screen flex flex-col items-center justify-center bg-black text-white">
+                <Particles />
 
                 <nav className="w-full py-6">
                     <div className="flex justify-center">
@@ -31,21 +31,22 @@ const Hero = () => {
 
                     <div className='mx-52'>
 
-                        <h2 className="text-5xl font-bold mb-4 z-10">
+                        <h2 className="text-5xl font-medium mb-4 z-10">
                             Revolutionizing travel with <span className="block">AI and personalized experiences</span>
                         </h2>
-                        <p className="mb-8 z-10">
-                            Our innovative AI and personalized approach redefines how you explore the world,
+                        <p className="mb-8 z-10 text-gray-300 font-semibold">
+                            Our innovative <span className='text-white'>AI and personalized approach</span> redefines how you explore the world,
                             <span className="block">so you can focus on creating unforgettable memories.</span>
                         </p>
                         <div className="space-x-2 mb-6 z-10">
                             <input
                                 type="email"
                                 placeholder="Enter your email to get notified"
-                                className="active px-4 py-2 rounded text-gray-200 bg-black"
+                                className="w-72 px-4 py-2 rounded text-gray-200 bg-black border border-transparent focus:border-white  focus:ring-2 ring-white border-white "
                             />
                             <button className="px-4 py-2 bg-white text-black rounded">Get Notified</button>
                         </div>
+
 
                     </div>
 
